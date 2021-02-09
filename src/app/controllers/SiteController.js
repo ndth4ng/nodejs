bb0036d8a50980ab8097dc03ff7d2d85964efe76
Thangee <ndth4ng@users.noreply.github.com>
2021-02-09 22:39:24 +0700

    [CRUD] Read from Database and Create data from Website

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,17 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-        Course.find({}, function (err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'ERROR!!!' });
-            }
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                // Chuyển model từ db thành object do bảo mật của handlebars
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
 
         //res.render('news');
     }
