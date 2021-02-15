f26e380d0c0f86e79d73b3d2a2693e16cece7213
Thangee <ndth4ng@users.noreply.github.com>
2021-02-15 23:59:12 +0700

    [CRUD] Force Delete Added

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -20,11 +20,13 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image =
+        req.body.image =
             'https://img.youtube.com/vi/' + req.body.videoId + '/sddefault.jpg';
-        const course = new Course(formData);
-        course.save().then(() => res.redirect('/'));
+        const course = new Course(req.body);
+        course
+            .save()
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch((error) => {});
     }
 
     // [GET] /courses/:id/edit
@@ -45,9 +47,23 @@ class CourseController {
             .catch(next);
     }
 
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
     // [DELETE] /courses/:id
     destroy(req, res, next) {
         Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
